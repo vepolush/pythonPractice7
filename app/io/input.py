8f06e12d62428f579d4edd3cb0936782bd3b1d98
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-03-27 10:04:50 +0200

    Functions created for work with files.

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas
+
+
+def inout_text_from_console(text):
+    pass
+
+
+def read_file(file_path):
+    pass
+
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_text_to_console(text):
+    pass
+
+
+def write_to_the_file(file_path):
+    pass
